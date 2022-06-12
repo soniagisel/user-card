8808e77e9f4e5fcb38939031f695425d3b55f824
Sonia Pepe <sogispe@gmail.com>
2022-06-12 23:01:50 +0300

    Added animation to card

--- src/Components/UserDetailsItem.js
+++ src/Components/UserDetailsItem.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styles from './UserDetailsItem.module.scss'
+import clsx from 'clsx'
 
 const UserDetailsItem = ({ onClickCallback, itemIcon, itemInfo, showPointer }) => {
     return (
-        <div onClick={onClickCallback} className={showPointer ? `${styles.showPointer}` : ''}>
+        <div
+            onClick={onClickCallback}
+            className={clsx(styles.itemContainer, showPointer ? `${styles.showPointer}` : '')}
+        >
             <FontAwesomeIcon icon={itemIcon} className={styles.icons} />
             <span title={itemInfo}>{itemInfo}</span>
         </div>
